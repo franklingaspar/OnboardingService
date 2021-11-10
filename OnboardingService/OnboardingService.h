7ea22c5887051d6607b9dfb728b5fad86401aff8
Franklin Gaspar <franklin.gaspar@hotmail.com>
2021-11-10 13:32:12 -0300

    Initial Commit

---
+++ OnboardingService/OnboardingService.h
@@ -0,0 +1,18 @@
+//
+//  OnboardingService.h
+//  OnboardingService
+//
+//  Created by magna on 10/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OnboardingService.
+FOUNDATION_EXPORT double OnboardingServiceVersionNumber;
+
+//! Project version string for OnboardingService.
+FOUNDATION_EXPORT const unsigned char OnboardingServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OnboardingService/PublicHeader.h>
+
+
