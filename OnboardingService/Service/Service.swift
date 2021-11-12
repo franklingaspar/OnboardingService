e7bafee5d5e159c2bdaa6f7a79a320c5dfc32207
Franklin Gaspar <franklin.gaspar@hotmail.com>
2021-11-12 15:06:02 -0300

    Consuming Models api

--- OnboardingService/Service/Service.swift
+++ OnboardingService/Service/Service.swift
@@ -1,33 +1,33 @@
-////
-////  Service.swift
-////  OnboardingService
-////
-////  Created by magna on 11/11/21.
-////
 //
-//import Foundation
+//  Service.swift
+//  OnboardingService
 //
-//public class Service {
-//  // MARK: Properties
-//    let service: OnboardingServiceClientDelegate
+//  Created by magna on 11/11/21.
 //
-//
-//    let login = Constants.login.rawValue
-//    let register = Constants.register.rawValue
-//    let refreshToken = Constants.refreshToken.rawValue
-//    let projects = Constants.projects.rawValue
-//
-//
-//    // MARK: Computed variables
-//    var brandURLName: String {
-//    return Constants.baseUrl.rawValue
-//    
-//    }
-//    
-//    init() {
-//        self.service = OnboardingServiceClient()
-//    }
-//
-//}
-//
-//extension Service: OnboardingDelegate {}
+
+import Foundation
+
+public class Service {
+  // MARK: Properties
+    let service: OnboardingServiceClientDelegate
+
+
+    let login = Constants.login.rawValue
+    let register = Constants.register.rawValue
+    let refreshToken = Constants.refreshToken.rawValue
+    let projects = Constants.projects.rawValue
+
+
+    // MARK: Computed variables
+    var brandURLName: String {
+    return Constants.baseUrl.rawValue
+    
+    }
+    
+    init() {
+        self.service = OnboardingServiceClient()
+    }
+
+}
+
+extension Service: OnboardingDelegate {}
